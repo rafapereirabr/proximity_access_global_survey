10b0efa613c8585802b5e0fe04061fe6d0126e55
Marrekoo <140586644+Marrekoo@users.noreply.github.com>
2023-11-27 10:51:17 +0100

    Processed Cecilia's requests
    Legend text has changed, and the order of the stacked bars. However, especially for the continents, it took quite some coding. Adding a geom object to a graph overrides all ordering statements.

--- R/11 figure 4.R
+++ R/11 figure 4.R
@@ -24,30 +24,30 @@ df_global <- inner_join(df_global,dist_conv, by=c("q4_nearby_distance"="distance
 # Define the city size classes and their corresponding labels
 
 # Some surveys have multiple answers, these will be removed.
-# If this part is commented out, the response will be linked to the largest size reported.
-
 
 df_global$sum_replies <- rowSums(df_global[,2:6])
-df_global <- filter(df_global, sum_replies==1)
+df_global <- filter(df_global, sum_replies==1)  # If this statement is commented out, the response will be linked to the largest size reported.
 
 # First we add a column, assuming that the largest size class is counted (multiple entries)
 
 df_global$size_class <- case_when(
   df_global[6]==TRUE ~ 1, # Small cities
-  df_global[5]==TRUE ~ 2,  # Medium cities
+  df_global[5]==TRUE ~ 2, # Medium cities
   df_global[4]==TRUE ~ 3, # Large cities
   df_global[3]==TRUE ~ 4, # Very large cities
-  df_global[2]==TRUE ~ 5 # Megacities
+  df_global[2]==TRUE ~ 5  # Megacities
 )
 
+# An attempt to create labels in the dataframe, it does not help to force the order of bars.
 
-
-city_sizes <- 1:5
-city_labels <- c("Small Cities (<100,000)",
-                 "Medium Cities (100,000-250,000)",
-                 "Large Cities (250,000-500,000)",
-                 "Very Large Cities (500,000-10,000,000)",
-                 "Megacities (>10,000,000)")
+city_sizes <- 0:5
+city_labels <- c(sprintf("All, n=%d", length(df_global$size_class)),
+                 sprintf("Small, n=%d", length(which(df_global$size_class==1))),
+                 sprintf("Medium, n=%d", length(which(df_global$size_class==2))),
+                 sprintf("Large, n=%d", length(which(df_global$size_class==3))),
+                 sprintf("Very large, n=%d", length(which(df_global$size_class==4))),
+                 sprintf("Mega, n=%d", length(which(df_global$size_class==5)))
+                 )
 cs_conv <- data.frame(city_sizes, city_labels)
 
 df_global <- inner_join(df_global, cs_conv, by=c("size_class"="city_sizes"))
@@ -58,23 +58,31 @@ df_global <- inner_join(df_global, cs_conv, by=c("size_class"="city_sizes"))
 
 figure_4_plot_stackedbar <- ggplot() +
   geom_bar(data = df_global,
-           position = position_fill(reverse=TRUE),
-           aes(x=factor(sum_replies),
+           position = position_fill(reverse=TRUE), # from high to low
+           aes(x=factor(sum_replies/sum_replies), # did this to force it to be 1, else it would end up in another stack
                fill=factor(distance_thresholds)
-               )
+               ),
+           width=0.9
            ) +
-
   geom_bar(data = df_global,
            position = position_fill(reverse=TRUE),
            aes(x=factor(size_class+1),
                fill=factor(distance_thresholds)
-               )
+               ),
+           width=0.5
            ) +
-  scale_x_discrete(labels=c("1" = "All", "2" = "Small", "3" = "Medium", "4" = "Large", "5" = "Very large", "6" = "Mega"))+
+  scale_x_discrete(labels=c("1" = cs_conv[1,2],
+                            "2" = cs_conv[2,2],
+                            "3" = cs_conv[3,2],
+                            "4" = cs_conv[4,2],
+                            "5" = cs_conv[5,2],
+                            "6" = cs_conv[6,2]))+
   theme_bw()+
-  theme(axis.text.x=element_text(angle=90,hjust=1))+
+  theme(axis.text.x=element_text(angle=20,hjust=1))+
   labs(y="Proportion of repsonses", x="City Size", fill="distance class upper limit")+
   scale_fill_grey(guide=guide_legend(reverse=TRUE))
 
+# I had to use an index to get all bars into one graph in the right order, and added 1 because '1' was already taken by 'all replies'.
+# The scale_x_discrete statement forces the labels of the columns.
 
 print(figure_4_plot_stackedbar)
--- R/12 figure 5.R
+++ R/12 figure 5.R
@@ -1,4 +1,4 @@
-# Making figure 5: contintnents
+# Making figure 5: continents
 
 # stacked bars by city size from the practitioners data
 
@@ -7,6 +7,7 @@
 library(ggplot2)
 library(tidyr)
 library(dplyr)
+library(forcats)
 
 # Read in raw data for both sets
 
@@ -33,34 +34,103 @@ df_global <- inner_join(df_global,dist_conv, by=c("q4_nearby_distance"="distance
 #South-East Europe & Middle East: Greece, Hungary, Turkey, Israel
 #Southern Europe":France, Italy, Portugal, Spain
 
-countries <- c("KENYA", "TUNISIA",
+countries <- c(   "ALL",
+                  "KENYA", "TUNISIA",
                   "CHINA",
                   "USA", "CANADA",
                   "ARGENTINA", "BRAZIL", "CHILE", "URUGUAY",
                   "FINLAND", "GERMANY", "NETHERLANDS", "POLAND", "NORWAY",
                   "GREECE", "HUNGARY", "TURKEY", "ISRAEL",
                   "FRANCE", "ITALY", "PORTUGAL", "SPAIN")
-continents <- c("1. Africa", "1. Africa",
-                   "2. Asia",
-                   "3. North America", "3. North America",
-                   "4. South America", "4. South America","4. South America","4. South America",
-                   "5. Northern Europe","5. Northern Europe","5. Northern Europe","5. Northern Europe","5. Northern Europe",
-                   "6. South-East Europe & Middle East", "6. South-East Europe & Middle East","6. South-East Europe & Middle East","6. South-East Europe & Middle East",
-                   "7. Southern Europe","7. Southern Europe","7. Southern Europe","7. Southern Europe")
+continents <- c(0,
+                1,1,
+                2,
+                3,3,
+                4,4,4,4,
+                5,5,5,5,5,
+                6,6,6,6,
+                7,7,7,7)
 
 cont_conv <- data.frame(countries,continents)
 
 df_global <- inner_join(df_global,cont_conv, by=c("country"="countries"))
-df_global$firstbar <- "0. All"
+df_global$firstbar <- "0" # To get the right order
+
+# Calculating the cutoff value to order the continents
+
+summarydata_dist <- df_global %>% count(continents,distance_thresholds, name="count")
+summarydata_dist <- summarydata_dist %>% group_by(continents) %>% mutate(cum_sum = cumsum(count))
+summarydata_cont <- df_global %>% count(continents)
+summarydata_dist <- inner_join(summarydata_dist, summarydata_cont, by = "continents")
+summarydata_dist$cumrel <- summarydata_dist$cum_sum/summarydata_dist$n
+summarydata_cutoff <- filter(summarydata_dist, distance_thresholds %in% "1200")
+
+
+df_global <- inner_join(df_global, select(summarydata_cutoff, continents, cumrel), by= "continents")
+
+# A table where the continents get the sample size
+
+cont.numbers <- 0:7
+cont.labs <- c(sprintf("All, n=%d", length(df_global$firstbar)),
+                          sprintf("Africa, n=%d", summarydata_cont[1,2]),
+                          sprintf("Asia, n=%d", summarydata_cont[2,2]),
+                          sprintf("N America, n=%d", summarydata_cont[3,2]),
+                          sprintf("S America, n=%d", summarydata_cont[4,2]),
+                          sprintf("N Europe, n=%d", summarydata_cont[5,2]),
+                          sprintf("SE Europe & ME, n=%d", summarydata_cont[6,2]),
+                          sprintf("S Europe, n=%d", summarydata_cont[7,2])
+                          )
+
+df_cont.labs <- data.frame(cont.numbers, cont.labs)
+
+df_global <- inner_join(df_global, df_cont.labs, by=c("continents"="cont.numbers"))
+
+# Now, the data needs to be sorted
+
+summarydata_cutoff <- inner_join(summarydata_cutoff,df_cont.labs, by=c("continents"="cont.numbers"))
+summarydata_cutoff <- summarydata_cutoff[order(summarydata_cutoff$cumrel),]
+
+# We now renumber the continents from 1 to 7
+
+summarydata_cutoff$continents2 <- 1:7
+
+# And reassign these new numbers to the df_global table
+
+df_global <- merge(df_global, select(summarydata_cutoff, continents, continents2),
+                by.x = "continents",
+                by.y = "continents",
+                all.x = TRUE,
+                sort = FALSE)
+
+# Also reassign these new numbers to the df_cont.labs dataframe for the labels
+
+df_cont.labs <- merge(df_cont.labs, select(summarydata_cutoff, continents, continents2),
+                      by.x = "cont.numbers",
+                      by.y = "continents",
+                      all.x = TRUE,
+                      sort = FALSE)
+
+df_cont.labs$continents2[df_cont.labs$cont.numbers == 0] <- 0 # The "All" label
+
+df_cont.labs <- df_cont.labs[order(df_cont.labs$continents2),] # Now they are in order of the 1200 meter cumulative percentile
+
 
 # The plotting begins
 
-plot_5_stacked_bars <- ggplot()+
-  geom_bar(data=df_global, aes(x=factor(firstbar), fill=factor(distance_thresholds)), position=position_fill(reverse=TRUE), width = 0.8)+
-  geom_bar(data=df_global, aes(x=factor(continents), fill=factor(distance_thresholds)), position=position_fill(reverse=TRUE), width = 0.3)+
+plot_5_stacked_bars <- ggplot(data=df_global, x=fct_reorder(cont.labs,factor(cumrel)))+
+  geom_bar(data=df_global, aes(x=factor(firstbar), fill=factor(distance_thresholds)), position=position_fill(reverse=TRUE), width = 0.9)+
+  geom_bar(data=df_global, aes(x=factor(continents2), fill=factor(distance_thresholds)), position=position_fill(reverse=TRUE), width = 0.5)+
   theme_bw() +
+  scale_x_discrete(labels=c("0" = df_cont.labs[1,2],
+                            "1" = df_cont.labs[2,2],
+                            "2" = df_cont.labs[3,2],
+                            "3" = df_cont.labs[4,2],
+                            "4" = df_cont.labs[5,2],
+                            "5" = df_cont.labs[6,2],
+                            "6" = df_cont.labs[7,2],
+                            "7" = df_cont.labs[8,2]))+
   scale_fill_grey(guide=guide_legend(reverse=TRUE))+
-  theme(axis.text.x=element_text(angle=20,hjust=1))+
+  theme(axis.text.x=element_text(angle=45,hjust=1))+
   labs(y="Proportion of repsonses", x="Continent", fill="distance class upper limit")
 
 
